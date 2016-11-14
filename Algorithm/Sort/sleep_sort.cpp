755837bcce939a931345531600804510423c0c06
刘俊延 <13925588730@163.com>
2016-11-14 12:24:47 +0800

    Update sleep_sort.cpp

--- Algorithm/Sort/sleep_sort.cpp
+++ Algorithm/Sort/sleep_sort.cpp
@@ -2,17 +2,17 @@
 #include <thread>
 int main()
 {
+int main() {
 	std::vector<int> v;
 	srand((int)time(0));
 	for (int i = 0; i < 50; ++i)	
-    v.push_back(rand());
+		v.push_back(rand());
 	std::cout << " before sort :";
 	for (auto& it : v)	
-    std::cout << " " << it;
+		std::cout << " " << it;
 	const int size = v.size();
 	std::vector<std::thread> threads;
-	for (int i = 0; i < size; ++i)
-  {
+	for (int i = 0; i < size; ++i) {
 		threads.push_back(std::thread([v,i](){std::this_thread::sleep_for(
 			std::chrono::milliseconds(v[i]));
 			std::cout << " " << v[i];
@@ -20,7 +20,7 @@ int main()
 	}
 	std::cout << std::endl << " after sort :";
 	clock_t start = clock();
-	for (auto& it : threads)
-		it.join();
+	for (auto& t : threads)
+		t.join();
 	std::cout << std::endl << " runtimes " << clock() - start << "ms" << std::endl;
 }
