24332c5ae5e8067612870cd5a233de5d7144edea
刘俊延 <13925588730@163.com>
2016-11-15 00:01:27 +0800

    Update sleep_sort.cpp

--- Algorithm/Sort/sleep_sort.cpp
+++ Algorithm/Sort/sleep_sort.cpp
@@ -1,8 +1,19 @@
+/*
+  sleep sort
+  Time complexity : none
+  Create n threads for n number, let the nth thread sleep a time as long as the nth number,
+  then ouput it. So the bigger the number, the later the output.
+  But, this method is only suitable for small data size, and these data are almost non-repetitive.
+  Otherwise, there will be errors in sorting.
+  For entertainment only!
+*/
+
+
 #include <iostream>
 #include <thread>
+
 int main()
 {
-int main() {
 	std::vector<int> v;
 	srand((int)time(0));
 	for (int i = 0; i < 50; ++i)	
