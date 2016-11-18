13437dbe68c8dbebb443d2dbf26c269213b09e78
刘俊延 <13925588730@163.com>
2016-11-18 23:06:10 +0800

    Update diamond.h

--- Draw/Draw/diamond.h
+++ Draw/Draw/diamond.h
@@ -5,25 +5,25 @@
 #include <iostream>
 
 /*
-	[description]
-	draw a pictrue like this: 
-			    1
-		      2 2 2
-			3 3 3 3 3
-			  .....
-	n n n ... n n n ... n n n 
-			  .....
-			3 3 3 3 3
-			  2 2 2
-				1
-	[note]
-	n should be in [0, 9], otherwise, other characters will be output
+  [description]
+  draw a pictrue like this: 
+              1
+            2 2 2
+          3 3 3 3 3
+            .....
+  n n n ... n n n ... n n n 
+            .....
+          3 3 3 3 3
+            2 2 2
+              1
+  [note]
+  n should be in [0, 9], otherwise, the behavior is unexpected.
 */
 void diamond_digit(int n) 
 {
-	for (int i = 1, flag = 1; i != 0; ++flag, i += (flag <= n) ? 1 : -1)
-		for (int j = 1; j <= 2 * (n + i) - 2; 
-			putchar(j < 2 * (n + i) - 2 ? (abs(2 * n - 1 - j) < i * 2 && j & 1) ? i + '0' : ' ' : '\n'), ++j) { }
+  for (int i = 1, flag = 1; i != 0; ++flag, i += (flag <= n) ? 1 : -1)
+    for (int j = 1; j <= 2 * (n + i) - 2; 
+      putchar(j < 2 * (n + i) - 2 ? (abs(2 * n - 1 - j) < i * 2 && j & 1) ? i + '0' : ' ' : '\n'), ++j) { }
 }
 
 #endif // !LCPP_DRAW_DIAMOND_H__
