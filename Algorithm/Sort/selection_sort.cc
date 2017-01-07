6d815a30e80f50b8b25b9242c8adc4a547f858bf
刘俊延 <13925588730@163.com>
2017-01-08 00:14:39 +0800

    Update and rename select_sort.cc to selection_sort.cc

--- Algorithm/Sort/selection_sort.cc
+++ Algorithm/Sort/selection_sort.cc
@@ -1,25 +1,32 @@
 /*
-  [ Select sort ]
-  T(n) = O(n²)
-  Iterator Required : Random access iterator
+  [ Selection sort ]
+  Best time complexity    : O(n²)
+  Worst time complexity   : O(n²)
+  Average time complexity : O(n²)
+  Iterator Required       : Random access iterator
 */
 #include <algorithm>
 #include <vector>
 #include <iostream>
+#include <ctime>
 
 #include <Windows.h>
 
 // function template
 template <typename Iter>
-void select_sort(Iter first, Iter last) {
-    if (first == last || first == last - 1) 
+void selection_sort(Iter first, Iter last)
+{
+    if (first == last || first == last - 1)
         return;
-    while (first != last) {
+    while (first != last)
+    {
         auto value = *first;
         auto i = first;
         auto j = first;
-        for (; i != last; ++i) {
-            if (*i < value) {
+        for (; i != last; ++i)
+        {
+            if (*i < value)
+            {
                 value = *i, j = i;
             }
         }
@@ -29,33 +36,47 @@ void select_sort(Iter first, Iter last) {
 }
 
 // test
-#define SELECT_SORT_TEST(count) do {                      \
+#define SELECTION_SORT_TEST(count) do {                   \
   std::vector<int> v(count);                               \
   for (auto& it : v) it = rand();                          \
   LARGE_INTEGER t1, t2, tc;                                \
   QueryPerformanceFrequency(&tc);                          \
   QueryPerformanceCounter(&t1);                            \
-  select_sort(v.begin(), v.end());                      \
+  selection_sort(v.begin(), v.end());                      \
   QueryPerformanceCounter(&t2);                            \
   printf(" %7d numbers cost : %fs\n",                      \
     count,(t2.QuadPart - t1.QuadPart)*1.0 / tc.QuadPart);  \
 } while(0)
 
-int main() {
-    std::vector<int> v{ 2,4,6,2,9,8,5,1,0,3 };
-    select_sort(v.begin(), v.end());
+int main() 
+{
+    srand((int)time(0));
+
+    // [ small data test ]
+    std::vector<int> v = { 2,3,6,9,0,3,9,6,5,7 };
+    selection_sort(v.begin(), v.end());
     for (auto& it : v)
         std::cout << " " << it;
     std::cout << "\n";
     // output:
-    // 0 1 2 2 3 4 5 6 8 9
+    // 0 2 3 3 5 6 6 7 9 9
 
-    SELECT_SORT_TEST(10000);
-    SELECT_SORT_TEST(100000);
-    SELECT_SORT_TEST(1000000);
+    // [ big data test ]
+    std::vector<int> v2(10000);
+    for (auto& it : v2)
+        it = rand();
+    selection_sort(v2.begin(), v2.end());
+    std::cout << std::boolalpha << " " << std::is_sorted(v2.begin(), v2.end()) << "\n";
+    // output:
+    // true
+
+    // [ performance test ]
+    SELECTION_SORT_TEST(10000);
+    SELECTION_SORT_TEST(100000);
+    SELECTION_SORT_TEST(1000000);
 }
 
-// for the SELECT_SORT_TEST, I test 5 times...It took a really long time!!!
+// for the SELECTION_SORT_TEST, I test 5 times...It took a really long time!
 
 // [ 1st time ]
 //   10000 numbers cost : 0.040697s
