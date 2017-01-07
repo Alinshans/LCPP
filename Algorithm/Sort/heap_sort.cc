1deb9eba73d53b60432fabc2fdeb1d0169eb7bb8
刘俊延 <13925588730@163.com>
2017-01-07 23:47:42 +0800

    Update heap_sort.cc

--- Algorithm/Sort/heap_sort.cc
+++ Algorithm/Sort/heap_sort.cc
@@ -1,12 +1,15 @@
 /*
   [ Heap sort ]
-  T(n) = O(nlogn)
-  Iterator Required : Random access iterator
+  Best time complexity    : O(nlog(n))
+  Worst time complexity   : O(nlog(n))
+  Average time complexity : O(nlog(n))
+  Iterator Required       : Random access iterator
 */
 
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <iostream>
+#include <ctime>
 
 #include <Windows.h>
 
@@ -55,25 +58,27 @@ void heap_sort(Iter first, Iter last)
 
 int main()
 {
-    // [ small data ]
-    std::vector<int> v = { 4,3,1,6,4,3,7,9,5,7 };
+    srand((int)time(0));
+  
+    // [ small data test ]
+    std::vector<int> v = { 2,3,6,9,0,3,9,6,5,7 };
     heap_sort(v.begin(), v.end());
     for (auto& it : v)
         std::cout << " " << it;
     std::cout << "\n";
     // output:
-    // 1 3 3 4 4 5 6 7 7 9
+    // 0 2 3 3 5 6 6 7 9 9
 
-    // [ big data ]
+    // [ big data test ]
     std::vector<int> v2(10000);
     for (auto& it : v2)
         it = rand();
     heap_sort(v2.begin(), v2.end());
-    std::cout << std::boolalpha << std::is_sorted(v2.begin(), v2.end()) << "\n";
+    std::cout << std::boolalpha << " " << std::is_sorted(v2.begin(), v2.end()) << "\n";
     // output:
     // true
 
-    // performance test
+    // [ performance test ]
     HEAP_SORT_TEST(10000);
     HEAP_SORT_TEST(100000);
     HEAP_SORT_TEST(1000000);
