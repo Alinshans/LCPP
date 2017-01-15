fabf7d9823e983604cb380cd2b7adf3d8844da82
刘俊延 <13925588730@163.com>
2017-01-15 14:17:26 +0800

    Refactoring and retest

--- Algorithm/Sort/binary_tree_sort.cc
+++ Algorithm/Sort/binary_tree_sort.cc
@@ -4,9 +4,11 @@
   Worst time complexity   : O(nlog(n))
   Average time complexity : O(nlog(n))
   Space complexity        : O(n)
+  Stable                  : Yes
   Iterator Required       : Forward iterator
 */
 #include <algorithm>
+#include <functional>
 #include <vector>
 #include <iostream>
 #include <ctime>
@@ -16,40 +18,56 @@
 
 // For convenience and performance, I use std::multiset directly.
 
-// function template
-template <typename Iter>
-void binary_tree_sort(Iter first, Iter last)
+// binary_tree_sort
+template <typename Iter, typename Compare = std::less<>>
+void binary_tree_sort(Iter first, Iter last, Compare cmp = Compare())
 {
-    std::multiset<typename std::iterator_traits<Iter>::value_type> tree(first, last);
+    std::multiset<typename std::iterator_traits<Iter>::value_type, Compare> tree(first, last);
     std::copy(tree.begin(), tree.end(), first);
 }
 
-#define BINARY_TREE_SORT_TEST(count) do {                 \
-  std::vector<int> v(count);                               \
-  for (auto& it : v) it = rand();                          \
-  LARGE_INTEGER t1, t2, tc;                                \
-  QueryPerformanceFrequency(&tc);                          \
-  QueryPerformanceCounter(&t1);                            \
-  binary_tree_sort(v.begin(), v.end());                    \
-  QueryPerformanceCounter(&t2);                            \
-  printf(" %7d numbers cost : %fs\n",                      \
-    count,(t2.QuadPart - t1.QuadPart)*1.0 / tc.QuadPart);  \
+// test
+#define SMALL_SORT_TEST(sort, count) do {                  \
+    double t = 0.0;                                         \
+    std::vector<int> v(count);                              \
+    LARGE_INTEGER t1, t2, tc;                               \
+    for (int i = 0; i < 10; ++i) {                          \
+        for (auto& it : v) it = rand();                     \
+        QueryPerformanceFrequency(&tc);                     \
+        QueryPerformanceCounter(&t1);                       \
+        sort(v.begin(), v.end());                           \
+        QueryPerformanceCounter(&t2);                       \
+        t += (t2.QuadPart - t1.QuadPart)*1e6 / tc.QuadPart; \
+    }                                                       \
+    t /= 10.0;                                              \
+    printf(" %7d numbers cost : %fμs\n", count, t);         \
+} while(0)
+
+#define LARGE_SORT_TEST(sort, count) do {                  \
+  std::vector<int> v(count);                                \
+  for (auto& it : v) it = rand();                           \
+  LARGE_INTEGER t1, t2, tc;                                 \
+  QueryPerformanceFrequency(&tc);                           \
+  QueryPerformanceCounter(&t1);                             \
+  sort(v.begin(), v.end());                                 \
+  QueryPerformanceCounter(&t2);                             \
+  printf(" %7d numbers cost : %fms\n",                      \
+    count,(t2.QuadPart - t1.QuadPart)*1e3 / tc.QuadPart);   \
 } while(0)
 
 int main()
 {
     srand((int)time(0));
 
-    // [ small data test ]
+    /** [ Correctness verification ] **/
     std::vector<int> v = { 2,3,6,9,0,3,9,6,5,7 };
-    binary_tree_sort(v.begin(), v.end());
+    binary_tree_sort(v.begin(), v.end(), std::greater<>());
     for (auto& it : v)
         std::cout << " " << it;
     std::cout << "\n";
     // output:
-    // 0 2 3 3 5 6 6 7 9 9
+    // 9 9 7 6 6 5 3 3 2 0
 
-    // [ big data test ]
     std::vector<int> v2(10000);
     for (auto& it : v2)
         it = rand();
@@ -58,40 +76,104 @@ int main()
     // output:
     // true
 
-    // [ performance test ]
-    BINARY_TREE_SORT_TEST(10000);
-    BINARY_TREE_SORT_TEST(100000);
-    BINARY_TREE_SORT_TEST(1000000);
+    /** [ Small amount of data ] **/
+    std::cout << "\n [ Small amount of data ]\n";
+    SMALL_SORT_TEST(binary_tree_sort, 16);
+    SMALL_SORT_TEST(binary_tree_sort, 32);
+    SMALL_SORT_TEST(binary_tree_sort, 64);
+    SMALL_SORT_TEST(binary_tree_sort, 128);
+    SMALL_SORT_TEST(binary_tree_sort, 512);
+    SMALL_SORT_TEST(binary_tree_sort, 1024);
+
+    /** [ Large amount of data ] **/
+    std::cout << "\n [ Large amount of data ]\n";
+    LARGE_SORT_TEST(binary_tree_sort, 10000);
+    LARGE_SORT_TEST(binary_tree_sort, 100000);
+    LARGE_SORT_TEST(binary_tree_sort, 1000000);
 }
 
-// for the BINARY_TREE_SORT_TEST, I test 5 times.
+// I run 5 times for 'Small amount of data' and 'Large amount of data'.
 
 // [ 1st time ]
-//   10000 numbers cost : 0.003503s
-//  100000 numbers cost : 0.065074s
-// 1000000 numbers cost : 1.458672s
+// [ Small amount of data ]
+//      16 numbers cost : 3.825565μs
+//      32 numbers cost : 8.397583μs
+//      64 numbers cost : 15.628834μs
+//     128 numbers cost : 33.123798μs
+//     512 numbers cost : 138.000274μs
+//    1024 numbers cost : 253.467035μs
+//
+// [ Large amount of data ]
+//   10000 numbers cost : 3.124834ms
+//  100000 numbers cost : 48.196992ms
+// 1000000 numbers cost : 1208.592220ms
 
 // [ 2nd time ]
-//   10000 numbers cost : 0.003174s
-//  100000 numbers cost : 0.058337s
-// 1000000 numbers cost : 1.364507s
+// [ Small amount of data ]
+//      16 numbers cost : 3.592299μs
+//      32 numbers cost : 10.217059μs
+//      64 numbers cost : 16.421939μs
+//     128 numbers cost : 34.196823μs
+//     512 numbers cost : 134.361322μs
+//    1024 numbers cost : 234.012636μs
+//
+// [ Large amount of data ]
+//   10000 numbers cost : 3.054854ms
+//  100000 numbers cost : 47.756119ms
+// 1000000 numbers cost : 1207.914815ms
 
 // [ 3rd time ]
-//   10000 numbers cost : 0.003132s
-//  100000 numbers cost : 0.058386s
-// 1000000 numbers cost : 1.437054s
+// [ Small amount of data ]
+//      16 numbers cost : 3.778912μs
+//      32 numbers cost : 8.817462μs
+//      64 numbers cost : 15.675488μs
+//     128 numbers cost : 33.403718μs
+//     512 numbers cost : 129.556038μs
+//    1024 numbers cost : 263.310868μs
+//
+// [ Large amount of data ]
+//   10000 numbers cost : 3.135564ms
+//  100000 numbers cost : 46.943886ms
+// 1000000 numbers cost : 1215.437183ms
 
 // [ 4th time ]
-//   10000 numbers cost : 0.003939s
-//  100000 numbers cost : 0.066762s
-// 1000000 numbers cost : 1.448976s
+// [ Small amount of data ]
+//      16 numbers cost : 3.312380μs
+//      32 numbers cost : 7.837744μs
+//      64 numbers cost : 14.089277μs
+//     128 numbers cost : 29.904725μs
+//     512 numbers cost : 129.089506μs
+//    1024 numbers cost : 268.489378μs
+//
+// [ Large amount of data ]
+//   10000 numbers cost : 3.072116ms
+//  100000 numbers cost : 64.484104ms
+// 1000000 numbers cost : 1275.706633ms
 
 // [ 5th time ]
-//   10000 numbers cost : 0.003353s
-//  100000 numbers cost : 0.059279s
-// 1000000 numbers cost : 1.421921s
+// [ Small amount of data ]
+//      16 numbers cost : 3.685606μs
+//      32 numbers cost : 8.397583μs
+//      64 numbers cost : 15.675488μs
+//     128 numbers cost : 34.803315μs
+//     512 numbers cost : 143.178784μs
+//    1024 numbers cost : 236.718523μs
+//
+// [ Large amount of data ]
+//   10000 numbers cost : 3.698202ms
+//  100000 numbers cost : 49.869511ms
+// 1000000 numbers cost : 1204.850630ms
 
 // [ average ]
-//   10000 numbers cost : 0.003420s
-//  100000 numbers cost : 0.061694s
-// 1000000 numbers cost : 1.426226s
+// [ Small amount of data ]
+//      16 numbers cost : 3.638952μs
+//      32 numbers cost : 8.733486μs
+//      64 numbers cost : 15.498205μs
+//     128 numbers cost : 33.086476μs
+//     512 numbers cost : 134.837185μs
+//    1024 numbers cost : 251.199688μs
+//
+// [ Large amount of data ]
+//   10000 numbers cost : 3.217114ms
+//  100000 numbers cost : 51.450122ms
+// 1000000 numbers cost : 1222.500296ms
